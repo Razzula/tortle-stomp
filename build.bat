b08388087e174302c872f58fffcbdb87a8876da8
Razzula <jack02gillespie@gmail.com>
2024-01-04 21:49:35 +0000

    Organise files

--- build.bat
+++ build.bat
@@ -1,2 +1,2 @@
-pyinstaller --onefile --noconsole --icon=dark.ico --name=tortle-stomp --add-data="dark.ico:." main.py
-pyinstaller --onefile --icon=dark.ico --name=tortle-stomp_debug --add-data="dark.ico:." main.py
+pyinstaller --onefile --noconsole --icon=dark.ico --name=tortle-stomp --add-data="dark.ico:." src/main.py
+pyinstaller --onefile --icon=light.ico --name=tortle-stomp_debug --add-data="dark.ico:." src/main.py
